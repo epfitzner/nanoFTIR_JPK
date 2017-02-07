e94d7d03cd3db9af12c866e05df1f12c80ec6705
epfitzner <emanuel.pfitzner@fu-berlin.de>
2017-02-07 11:27:36 +0100

    Corrections

--- calcStdDev.m
+++ calcStdDev.m
@@ -31,19 +31,19 @@ function [stdDevComplex, stdDevAngle] = calcStdDev(spectrum,ref)
     end
         
     if nargin == 1
-        stdDevComplex = t*sqrt(1/size(spectrum,1))*std(spectrum,1);
+        stdDevComplex = t*sqrt(1/size(spectrum,1))*(std(real(spectrum),1)+1i*std(imag(spectrum),1));
         stdDevAngle = 0;
         
         return
     end
     
-    stdDev = t*sqrt(1/size(spectrum,1))*std(spectrum,1);
-    stdDevRef = t*sqrt(1/size(ref,1))*std(ref,1);
+    stdDev = t*sqrt(1/size(spectrum,1))*(std(real(spectrum),1)+1i*std(imag(spectrum),1));
+    stdDevRef = t*sqrt(1/size(ref,1))*(std(real(ref),1)+1i*std(imag(ref),1));
 
     spectrum = mean(spectrum,1);
     ref = mean(ref,1);
     
-    stdDevComplex = sqrt((stdDev./ref).^2 + (stdDevRef.*spectrum./ref.^2).^2);
+    stdDevComplex = sqrt((real(stdDev)./real(ref)).^2 + (-real(stdDevRef).*real(spectrum./(ref.^2))).^2)+1i.*sqrt((imag(stdDev)./imag(ref)).^2 + (-imag(stdDevRef).*imag(spectrum./(ref.^2))).^2);
     
     x = spectrum./ref;
     stdDevAngle = 1./(1+(real(x)./imag(x)).^2).*sqrt((real(stdDevComplex)./imag(x)).^2 + (imag(stdDevComplex).*real(x)./imag(x).^2).^2);
