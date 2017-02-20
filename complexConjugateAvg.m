cd070976ef32d7140dcf869a9f767124c329d275
epfitzner <emanuel.pfitzner@fu-berlin.de>
2017-02-20 16:17:10 +0100

    Corrected Shift for multidimensional spectra

--- complexConjugateAvg.m
+++ complexConjugateAvg.m
@@ -1,6 +1,6 @@
 function CC = complexConjugateAvg(spectrum)
 %%Calculate the average of a symmetric Complex conjugate spectrum
 
-    CC = (spectrum+fliplr(circshift(conj(spectrum),[-1 -1])))./2;
+    CC = (spectrum+fliplr(circshift(conj(spectrum),[-1 0])))./2;
 
 end
