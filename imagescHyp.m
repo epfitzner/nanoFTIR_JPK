6c412f9585ef223f61fd46cd607dcf98f6894e30
epfitzner <emanuel.pfitzner@fu-berlin.de>
2017-02-21 11:21:56 +0100

    added

---
+++ imagescHyp.m
@@ -0,0 +1,46 @@
+function imagescHyp( fw , bw , wnDisp , wn , refIdx )
+%IMAGESCHYP Summary of this function goes here
+%   Detailed explanation goes here
+    
+    if nargin < 5
+        refIdx = [];
+    end
+    
+    wnIdx = find(diff(sign(wn-wnDisp)));
+    
+    fw = complexConjugateAvg(fw,4);
+    bw = complexConjugateAvg(bw,4);
+    
+    fw = reshape(mean(fw(:,:,:,wnIdx),3),[size(fw,1) size(fw,2)]);
+    bw = reshape(mean(bw(:,:,:,wnIdx),3),[size(bw,1) size(bw,2)]);
+    
+    if numel(refIdx) == 0
+        fwRef = ones(size(fw,1),size(fw,2));
+        bwRef = ones(size(bw,1),size(bw,2));
+    else
+        fwRef = mean(mean(fw(refIdx(1,:),refIdx(2,:)),2),1);
+        bwRef = mean(mean(bw(refIdx(1,:),refIdx(2,:)),2),1);
+    end
+
+    figure
+    ax1 = subplot(2,2,1);
+    imagesc(abs(fw./fwRef))
+    colormap(ax1,hot)
+    colorbar
+    
+    ax2 = subplot(2,2,2);
+    imagesc(angle(fw./fwRef))
+    colormap(ax2,jet)
+    colorbar
+    
+    ax3 = subplot(2,2,3);
+    imagesc(abs(bw./bwRef))   
+    colormap(ax3,hot)
+    colorbar
+    
+    ax4 = subplot(2,2,4);
+    imagesc(angle(bw./bwRef))
+    colormap(ax4,jet)
+    colorbar
+end
+
